ef08fdcbce8ae25786cd0d8ffbe5a26933c8d58f
WILLIAN NUNES PARENTE <willianparente@hotmail.com>
2020-12-09 14:21:47 -0300

    Add home route

--- src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
+++ src/main/java/br/com/professorisidro/validacpf/controller/ValidadorController.java
@@ -11,6 +11,11 @@ import br.com.professorisidro.validacpf.service.Validador;
 @CrossOrigin("*")
 public class ValidadorController {
 	
+	@GetMapping("/")
+	public String index() {
+		return "{\"status\": \"Aplicação iniciada\"}";
+	}
+
 	@GetMapping("/validacpf")
 	public String validaCPF(@RequestParam String cpf) {
 		if (Validador.isCPF(cpf)) {
